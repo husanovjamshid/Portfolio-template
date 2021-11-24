4a11f7706bae729756001d7e67e7b9b22bb891cf
JamshidBek <jamshidbek4455@gmail.com>
2021-11-24 22:33:53 +0500

    Glozza Cafe sayti biriktirildi

---
+++ Glozza-Cafe/vakansiya.php
@@ -0,0 +1,171 @@
+<!doctype html>
+<html lang="Uz">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/fontawesome.css">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Glozza Cafe</title>
+
+</head>
+<body>
+
+<!-- NAVBAR -->
+
+<nav class="navbar head navbar-expand-sm  ">
+    <div class="container">
+        <ul class="navbar-nav ml-5">
+            <li class="nav-item">
+                <a class="nav-link text-primary" href="https://www.facebook.com/jamshidbek.husanov.7/">
+                    <i class="fab fa-facebook icon_fx"></i>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-primary" href="https://t.me/TerDU_104">
+                    <i class="fab fa-telegram icon_fx"></i>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-danger" href="https://www.instagram.com/_khusanov_o2_/">
+                    <i class="fab fa-instagram icon_fx "></i>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-primary" href="https://www.twitter.com/khusanov_o2">
+                    <i class="fab fa-twitter icon_fx "></i>
+                </a>
+            </li>
+        </ul>
+
+        <div class="navbar-brand ">
+            <a class="nav-link " href="#">
+                <img src="img/palma1.png" class="img-fluid " alt="">
+            </a>
+        </div>
+
+        <div class="form-group lang1 ">
+            <select class="lang" name="RU" id="">
+                <option  value="RU"><p>Ru</p></option>
+                <option value="UZ">Uz</option>
+                <option value="ENG">Eng</option>
+            </select>
+        </div>
+
+        <div class="icons">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link text-dark shop" href="#">
+                        <i class="fas icon_fx fa-shopping-basket " aria-hidden="true"></i>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-dark" href="#">
+                        <i class="far icon_fx fa-user"></i>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<!-- /// -->
+
+<div class="container py-5">
+    <div class="card vakansiy_card  " style="background-color: #F4F4F4;">
+        <div class=" card-body">
+            <div class="d-flex" style="background-color: white;padding: 20px;border-radius: 15px;">
+                <div class=" font-weight-bolder mt-1" style="font-weight: 900;font-size: 22px;">Повар</div>
+                <div class="vakansiy_img border"><img src="img/up_line.png" class="img-fluid" alt=""></div>
+            </div>
+        </div>
+
+        <div>
+            <div class="container">
+            <div class="row">
+                <div class="col-md-6 py-4">
+                    <p class="text-secondary">График работы:</p>
+                    <p class="text-secondary">Условия:</p>
+                    <br>
+                    <p class="text-secondary">Зарплата:</p>
+                    <p class="text-secondary">Обязанности:</p>
+
+                </div>
+                <div class="col-md-6 py-4">
+                    <p class="font-weight-bold ">Полный день</p>
+                    <p class="font-weight-bold ">з/п при  собеседовании</p>
+                    <br>
+                    <p class="font-weight-bold ">по соглашению</p>
+                    <p class="font-weight-bold">Работа в команде</p>
+                </div>
+            </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div id="rezyume">
+    <div class="container  py-5" style="margin-left: 320px">
+        <div>
+            <h1 class="font-weight-bold ml-5">Резюме</h1>
+            <p class="text-secondary ml-5">Отправьте нам ваше резюме</p>
+        </div>
+        <form class="form-group d-flex ml-5" action="">
+            <label for="ism"><span class="im_ya"> Имя</span>
+                <input type="text" id="ism" placeholder="Жамшидбек"  class="form-control border-0 input_1 " required>
+            </label><br>
+
+
+            <label ><span class="im_ya1">Номер</span>
+                <input type="tel" placeholder="+998 93 895-44-55" required class="form-control border-0 input_2 ">
+            </label>
+
+        </form>
+        <label for="text" ><Span class="area ml-5">Сообщение</Span></label><br>
+        <textarea  class="messages" name="Сообщения" id="text" cols="50" rows="4" placeholder="Ваше собщения "></textarea>
+
+        <div class="d-flex">
+            <div>
+                <p class="tell_num">Можете связаться по телефону:</p>
+                <a class="tell_num1" style="text-decoration: none;font-weight: 900" href="tel:+998938954455">+99893 895-44-55</a>
+            </div>
+
+            <button style="border-radius: 35px;" class="btn btn-success btn_right">Отправить сообщение</button>
+        </div>
+    </div>
+
+<!-- BUTTON -->
+
+<div id="button" class="buttom">
+    <div class="container py-5 mr-5 ">
+        <div class="row">
+            <div class="col-md-6 ">
+
+                <h4>— У вас возникли вопросы?! У моих друзей—операторов <br> всегда есть ответы! Звоните нам каждый день с 08:00 до
+                    <br> 23:00 По номеру: +998 93 895 44 55.
+                    <br><br><br>
+                    Мы с радостью поможем оформить вам заказ на вынос <br> или доставку. Доставка осуществляется по всему городу
+                    <br> Ташкент. Стоимость зависит от расстояния. Первые <br> 3 километра – 9 000 сум, после, каждый последующий по
+                    <br> 1 300 сум. Режим работы доставки: 09:00 – 22:00.</h4>
+
+            </div>
+            <div class="col-md-6">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12156.092153917025!2d71.780066!3d40.386182!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xed832a8fca5edb61!2sPalma%20Kafe!5e0!3m2!1sru!2sus!4v1616947173008!5m2!1sru!2sus" width="363" height="308" style="border:0; border-radius: 20px;" allowfullscreen="" loading="lazy"></iframe>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+<div class="pasti mb-0">
+    <h4><i class="fas fa-coffee"></i> Glozza Cafe</h4>
+</div>
+
+<script src="js/bootstrap.js"></script>
+<script src="js/fontawesome.js"></script>
+<script src="js/jquery.js"></script>
+<script src="js/popper.js"></script>
+</body>
+</html>
